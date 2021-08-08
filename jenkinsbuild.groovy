440589c093e9c9df1144dccc6e5da11b2396cc39
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-08-08 17:54:42 +0530

    Update jenkinsbuild.groovy

--- jenkinsbuild.groovy
+++ jenkinsbuild.groovy
@@ -3,7 +3,7 @@ pipeline{
   stages{
     stage('git checkout'){
       steps{
-        git branch: 'master', url: 'https://github.com/swetha8998/jenkinsbuild'     
+        git branch: 'master', url: 'https://github.com/swetha8998/jenkinsbuild.git'     
   }
 } 
     stage('Building'){
