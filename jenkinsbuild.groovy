4c51bb5415b7ec134605cca0c421924a53660541
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-08-08 18:06:57 +0530

    Update jenkinsbuild.groovy

--- jenkinsbuild.groovy
+++ jenkinsbuild.groovy
@@ -8,8 +8,7 @@ pipeline{
 } 
     stage('Building'){
    steps{
-       script{
-            
+       script{           
 sh 'docker build -t buildimg .'
 sh 'aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws'
 sh 'aws ecr-public create-repository \
