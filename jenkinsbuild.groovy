35a53323d1d550917bdc52785d2f4d4cecdbe68f
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-08-08 17:48:52 +0530

    Update jenkinsbuild.groovy

--- jenkinsbuild.groovy
+++ jenkinsbuild.groovy
@@ -1,1 +1,7 @@
-
+sh 'docker build -t buildimg .'
+sh 'aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws'
+sh 'aws ecr-public create-repository \
+     --repository-name build-image \
+     --region us-east-1'
+sh 'docker tag buildimg:latest public.ecr.aws/registry_alias/build-image'
+sh 'docker push public.ecr.aws/registry_alias/build-image'
